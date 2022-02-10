e8a6330d77ede497a289171ace8694d9bc214d00
tomatodayo <kantogoto3104@gmail.com>
2022-02-10 15:59:26 +0900

    Userモデル_テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,14 @@ class User < ApplicationRecord
   validates :kana_last_name,  presence: true
   validates :birth_day,       presence: true
 
+  PASSWORD_REGEX = /(?=.*?[a-z])(?=.*?[\d])/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  NAME = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  validates_format_of :first_name, with: NAME
+  validates_format_of :last_name, with: NAME
+  validates_format_of :kana_first_name, with: NAME
+  validates_format_of :kana_last_name, with: NAME
+
   has_many :products
   has_many :users
   
