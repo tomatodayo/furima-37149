67d1cfe299f0f424630ff4bf2114eb3b50e65476
tomatodayo <kantogoto3104@gmail.com>
2022-02-11 23:29:06 +0900

    Productのバリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,14 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :product_title,        presence: true
+  validates :product_concept,      presence: true
+  validates :product_category_id,  presence: true
+  validates :product_situation_id, presence: true
+  validates :send_load_id,         presence: true
+  validates :send_source_area_id,  presence: true
+  validates :send_day_id,          presence: true
+  validates :product_price,        presence: true
+  validates :image,                presence: true
+
 end
