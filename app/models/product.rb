9b97cb53ff6065fa18639a0113ade232c01a075e
tomatodayo <kantogoto3104@gmail.com>
2022-02-17 22:41:39 +0900

    ActiveHashの修正２

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,11 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :SendSourceArea
-  belongs_to :ProductCategory
-  belongs_to :SendDay
-  belongs_to :SendLoad
-  belongs_to :ProductSituation
+  belongs_to :product_category
+  belongs_to :product_situation
+  belongs_to :send_day
+  belongs_to :send_load
+  belongs_to :send_source_area
 
   validates :product_title,        presence: true, length: { minimum: 1, maximum: 40 }
   validates :product_concept,      presence: true, length: { minimum: 1, maximum: 1000 }
