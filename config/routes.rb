d6ebf70b474f68ddb7b9fbc9608a3fbb1f8c2d45
tomatodayo <kantogoto3104@gmail.com>
2022-02-18 15:00:15 +0900

    showのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root to: "products#index"
 
-  resources :products, only: [ :index, :new, :create]
+  resources :products, only: [ :index, :new, :create, :show]
 
 end
