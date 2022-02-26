7e4d8fef477b7b30825142eaa0283c16894305a2
tomatodayo <kantogoto3104@gmail.com>
2022-02-26 18:58:18 +0900

    FactoryBotでデータの生成

--- spec/factories/buy_addresses.rb
+++ spec/factories/buy_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :buy_address do
-    
+    post_code { '123-4567' }
+    prefecture_id { 4 }
+    city { '港区' }
+    block { '1-1' }
+    building { '東京ハイツ' }
+    phone_number {12345678902}
   end
 end
