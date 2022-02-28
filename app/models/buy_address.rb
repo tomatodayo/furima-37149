7f1296dfef1db9028b55f8a2a3dbf92d69a0ea6f
tomatodayo <kantogoto3104@gmail.com>
2022-02-28 17:02:22 +0900

    バリデーションの修正

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -9,11 +9,10 @@ class BuyAddress
     validates :prefecture_id, numericality: {other_than: 1}
     validates :city
     validates :block
-    validates :phone_number,  format: {with: /\A[0-9]{11}\z/}
+    validates :phone_number,  format: {with: /\A[0-9]{10,11}\z/}
     validates :token
   end
-  validate :building
-
+  
   def save
     buy = Buy.create(user_id: user_id, product_id: product_id)
     Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, buy_id: buy.id)
